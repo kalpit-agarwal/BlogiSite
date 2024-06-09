dc6292ba19b0521adc41212ed04a6bfda975a87e
kalpit-agarwal <108914877+kalpit-agarwal@users.noreply.github.com>
2024-06-09 20:29:02 +0530

    second

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,17 +1,27 @@
-// import { useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Header from "../../components/header/Header";
 import Posts from "../../components/posts/Posts";
 import Sidebar from "../../components/sidebar/Sidebar";
 import "./homepage.css";
+import axios from "axios";
 
 export default function Homepage() {
-  // const location = useLocation();
-  // console.log(location);
+  const [posts, setPosts] = useState([]);
+  // const { search } = useLocation();
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get("localhost:5000/api/posts/");
+      setPosts(res.data);
+    };
+    fetchPosts();
+  });
+
   return (
     <>
       <Header />
       <div className="home">
-        <Posts />
+        <Posts posts={posts} />
         <Sidebar />
       </div>
     </>
